453ff16cffde9d3e383da3284362341f97003a9a
parkershepherd <me@parkershepherd.com>
2025-06-03 23:45:31 -0600

    Prevent graph from being cut off on small screens

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import ResolutionCalculator from './components/ResolutionCalculator.vue'
 </script>
 
 <template>
-  <div class="max-w-9/10 py-8 sm:p-8 m-auto">
+  <div class="max-w-9/10 py-8 px-2 sm:p-8 m-auto min-w-xl">
     <ResolutionCalculator/>
   </div>
 </template>
